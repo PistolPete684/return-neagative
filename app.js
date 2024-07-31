560144ad35f98e7f431a69a2a3d5ff54e10d5ceb
Peter <pwlinton@mta.ca>
2024-07-30 22:16:50 -0300

    addingjs file

---
+++ app.js
@@ -0,0 +1,10 @@
+function makeNegative(num) {
+  //check if number is negative
+  if(num < 0) {
+    //return number un altered if not
+     return num 
+     } else {
+        //return num mulitiplied by -1 if not
+        return num * -1
+  }
+}
